ee07c19d7bd810e3a4681a7903dcc572cc5a30e3
RoitoHNW <68737301+RoitoHNW@users.noreply.github.com>
2021-02-11 22:05:30 +0900

    TEST

--- src/main/java/hnwblackjack/com/github/roitohnw/Com/github/roitohnw.java
+++ src/main/java/hnwblackjack/com/github/roitohnw/Com/github/roitohnw.java
@@ -11,6 +11,7 @@ import java.util.Random;
 public final class roitohnw extends JavaPlugin {
     Random r;
 
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -27,42 +28,37 @@ public final class roitohnw extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getLabel().equalsIgnoreCase("hbhelp"))
-            // (command.getLabel().equalsIgnoreCase("追加するコマンド"))
-            //これで2つ目のコマンドを追加できる
-            sender.sendMessage("/shb BJ開始コマンド\r\n/hb ダイスを2回振るコマンド");
-
-        if (command.getLabel().equalsIgnoreCase("shb"))
-            sender.sendMessage("対戦相手を招待してください");
-        if (!(sender instanceof Player)){
-            return true;
-            if (command.getLabel().equalsIgnoreCase("invite")){
-
+        if (command.getName().equalsIgnoreCase("hb")) {
+            if (args[0].equals("help")) {
+                sender.sendMessage("/hb BJ開始コマンド\r\n/hb ダイスを2回振るコマンド");
+                return true;
             }
-                if (args.length != 1)return true;
-                Player p = Bukkit.getPlayer(args[0]);
-                if (p == null){
+            if (args[0].equals("invite")) {
+                Player p = Bukkit.getPlayer(args[1]);
+                if (p == null) {
                     sender.sendMessage("このプレイヤーは存在しません!");
                     return true;
                 }
-                if (!p.isOnline()){
+
+                if (!p.isOnline()) {
                     sender.sendMessage("このプレイヤーはオフラインです！");
                     return true;
                 }
                 sender.sendMessage(p.getName() + "を招待しました");
                 p.sendMessage(sender.getName() + "から招待が届いています");
-                //etc...
+                return true;
+            }
         }
-
-
-        if (command.getName().equals("hbj")) {
-            int i = r.nextInt(13)+1;
-            int l = r.nextInt(13)+1;
+        if (args[0].equals("dice")) {
+            int i = r.nextInt(13) + 1;
+            int l = r.nextInt(13) + 1;
             int all = i + l;
-            Bukkit.broadcastMessage(sender.getName() + "は13面ダイスを2回振って" + i + "と" + l +"が出た" +  "先行は" + all +"です");
-            return  true;
+            Bukkit.broadcastMessage(sender.getName() + "は13面ダイスを2回振って" + i + "と" + l + "が出た" + "先行は" + all + "です");
+            return true;
+        } else {
+            sender.sendMessage("使い方が間違っています");
+            return true;
         }
-        return  true;
     }
 }
 
